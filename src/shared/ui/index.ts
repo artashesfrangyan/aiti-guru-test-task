9db212a32ff4edc9a1ca981839eb8d232953d283
artashesfrangyan <frangyan99@gmail.com>
2026-02-21 16:58:15 +0300

    refactor: экспорты shared UI

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,5 @@
+export { Input } from './input/Input';
+export { Toast } from './Toast';
+export { Button } from './button/Button';
+export { Checkbox } from './checkbox/Checkbox';
+export * from './icons';
